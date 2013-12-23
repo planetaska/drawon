6510096f61914a454c35a807b79480912eaf3f15
aska <planetaska@gmail.com>
2013-12-23 16:34:16 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drawon::Application.config.secret_key_base = '4ed31092fa4ae62a05be0fd260a7e25bf23334c9c60fbcb2a496376046933b3e34cbff31da22e8793183346002c19b8e57bbf719679737aa602bf44b407af69b'
